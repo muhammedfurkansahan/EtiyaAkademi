6c6658a26cac1eef957dec6842130bd77be1f3eb
MUHAMMED FURKAN ŞAHAN <99537893+muhammedfurkansahan@users.noreply.github.com>
2022-11-17 10:09:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#Githuba vscode üzerinden repo göndermek
+#python giris degiskenlere deger atama degiskenlerin neler oldugunu anlama vb.
+
+headline = "Welcome"  #string
+sure = 12  #int
+fratio1 = 7.47  #float
+fratio2 = 7.44
+fratio3 = 7.41
+
+print(headline)
+print(type(headline))
+print(type(sure))
+print(type(fratio))
+
+message = "Hoşgeldin"
+customerName = "Furkan"
+customerSurname = "Şahan"
+finalmessage = message + " " + customerName + " " + customerSurname + ":)"
+
+print(finalmessage)
+
+number1 = 10
+number2 = 20
+print(number1 + number2)
+
+print(finalmessage)
